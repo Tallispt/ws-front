3dc4dc2d2dfcb170b760e4627422f409f4061033
Tallispt <tallispt@gmail.com>
2023-05-29 17:26:19 -0300

    fix: took off global styles

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,5 @@ import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import GlobalStyle from './style/globalStyles';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-    <GlobalStyle />
-    <App />
-  </>
-);
+root.render(<App />);
