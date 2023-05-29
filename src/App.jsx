62c7a6f9342d6ec38f5aa7967206da0db6a2989b
Tallispt <tallispt@gmail.com>
2023-05-29 17:31:59 -0300

    feat: implemented token context

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react'
 
 import Main from './pages/Main'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 
+import { UserProvider } from './context/userContext';
+import useToken from './hooks/useToken';
+
 function App() {
   return (
     <ChakraProvider>
-      <Router>
-        <Routes>
-          <Route path='/sign-in' element={<SignIn/>}/>
-          <Route path='/sign-up' element={<SignUp/>}/>
-          
-          <Route path='/' element={<Main/>}/>
-        </Routes>
-      </Router>
+      <UserProvider>
+        <Router>
+          <Routes>
+            <Route path='/sign-in' element={<SignIn/>}/>
+            <Route path='/sign-up' element={<SignUp/>}/>
+            
+              <Route path='/' element={          
+              <ProtectedRouteGuard>
+                <Main/>
+              </ProtectedRouteGuard>
+              }/>
+
+          </Routes>
+        </Router>
+      </UserProvider>
     </ChakraProvider>
   );
+}
+
+function ProtectedRouteGuard({ children }) {
+  const token = useToken();
+
+  if (!token) {
+    return <Navigate to="/sign-in" />;
+  }
+
+  return <>{children}</>;
 }
 
 export default App;
---
+++ src/hooks/useToken.js
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import UserContext from '../context/userContext';
+
+
+
+export default function useToken() {
+  const { userData: user } = useContext(UserContext);
+
+  return user.token;
+}
