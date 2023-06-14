35b937b0f5f9a3b1a0701f3394ac0cb5c39e3fce
Tallispt <tallispt@gmail.com>
2023-06-14 17:38:13 -0300

    feat: redirect on click

--- src/components/MainLogo/index.jsx
+++ src/components/MainLogo/index.jsx
@@ -1,14 +1,15 @@
 import styled from "styled-components"
 import { themes } from "../../style/themes"
+import { Link } from "react-router-dom"
 
-const MainLogo = ({fontSize = '2.8rem' , theme = themes.default}) => {
+const MainLogo = ({fontSize = '2.8rem' , theme = themes.default, route = "analysis"}) => {
 
   return(
-    <Title fontSize={fontSize} theme={theme}>WS</Title>
+    <Title to={route} fontSize={fontSize} theme={theme}>WS</Title>
   )
 }
 
-const Title = styled.p`
+const Title = styled(Link)`
   font-size: ${props => props.fontSize};
   font-weight: bold;
   text-shadow: -1.5px 1.6px 0px ${props => props.theme.secundary};
