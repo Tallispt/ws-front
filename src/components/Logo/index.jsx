5a15e1f981fe24987ccaad517a998a4058f65e7c
Tallispt <tallispt@gmail.com>
2024-05-08 17:47:43 -0300

    feat: responsive auth page

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,27 +1,38 @@
 import styled from "styled-components";
-import MainLogo from "../MainLogo";
-import {themes} from "../../style/themes";
+import { themes } from "../../style/themes";
 
-const Logo = ({fontSize = '2.8rem', theme = themes.default}) => {
-
-  return(
+const Logo = ({ fontSize = "2.8rem", theme = themes.default }) => {
+  return (
     <Container>
-      <MainLogo fontSize={fontSize} theme={theme}/>
-      <Title fontSize={fontSize} theme={theme}> Metrix</Title>
+      <WS fontSize={fontSize} theme={theme}>
+        WS
+      </WS>
+      <Title fontSize={fontSize} theme={theme}>
+        {" "}
+        Metrix
+      </Title>
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.div`
   display: flex;
-`
+`;
 
 const Title = styled.p`
-  font-size: ${props => props.fontSize};
-  text-shadow: -1.5px 1.6px 0px ${props => props.theme.secundary};
+  font-size: ${(props) => props.fontSize};
+  text-shadow: -1.5px 1.6px 0px ${(props) => props.theme.secundary};
   font-weight: bold;
   white-space: break-spaces;
-  color: ${props => props.theme.main}
-`
+  color: ${(props) => props.theme.main};
+
+  @media (max-width: 510px) {
+    font-size: 3rem;
+  }
+`;
+
+const WS = styled(Title)`
+  letter-spacing: -0.33rem;
+`;
 
 export default Logo;
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,9 +1,17 @@
-import styled from "styled-components";
-import { Input, InputGroup, InputRightElement, Button, useToast, FormControl, useBoolean } from '@chakra-ui/react'
-import { RiEye2Line, RiEyeCloseLine } from 'react-icons/ri'
+import {
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+  useToast,
+  useBoolean,
+  Stack,
+  IconButton,
+} from "@chakra-ui/react";
+import { RiEye2Line, RiEyeCloseLine } from "react-icons/ri";
 import { Link, useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form"
-import { joiResolver } from '@hookform/resolvers/joi'
+import { useForm } from "react-hook-form";
+import { joiResolver } from "@hookform/resolvers/joi";
 
 import { colors } from "../../style/color";
 import useSignUp from "../../hooks/api/useSignup";
@@ -11,167 +19,152 @@ import SignPage from "../SignPage";
 import { signUpSchema } from "../../schemas/signUpSchema";
 
 const SignUp = () => {
-  const [show, setShow] = useBoolean()
-  const [showConfirmation, setShowConfirmation] = useBoolean()
+  const [show, setShow] = useBoolean();
+  const [showConfirmation, setShowConfirmation] = useBoolean();
 
   const toast = useToast();
   const navigate = useNavigate();
   const { signUp, signUpLoading } = useSignUp();
 
-  const { handleSubmit, register, formState: { errors, isSubmitting } } = useForm({
-    resolver: joiResolver(signUpSchema)
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: joiResolver(signUpSchema),
   });
 
   const onValidating = (e) => {
-    e && toast({ title: e.message, colorScheme: 'red' });
-  }
+    e && toast({ title: e.message, colorScheme: "red" });
+  };
 
-  const onSubmit = async data => {
-    delete data.repeatPassword
+  const onSubmit = async (data) => {
+    delete data.repeatPassword;
 
     try {
-      await signUp(data)
-      toast({ title: 'Inscrito com sucesso! Por favor, faça login' });
-      navigate('/sign-in');
+      await signUp(data);
+      toast({ title: "Inscrito com sucesso! Por favor, faça login" });
+      navigate("/sign-in");
     } catch (e) {
       toast({
-        title: 'Não foi possível fazer o cadastro!',
+        title: "Não foi possível fazer o cadastro!",
         description: e.response?.data?.error,
-        colorScheme: 'red'
+        colorScheme: "red",
       });
     }
-  }
+  };
 
   return (
     <SignPage>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Form>
-          <InputGroup
-            display={"flex"}
-            flexDirection={"column"}
-            gap={3}
-          >
+        <Stack spacing={[6, 8]}>
+          <InputGroup display={"flex"} flexDirection={"column"} gap={[2, 4]}>
             <Input
-              id='username'
+              id="username"
               placeholder="Nome do usuário"
-              {...register('username')}
+              {...register("username")}
               isRequired
               isInvalid={errors.username}
               onInvalid={() => onValidating(errors.username)}
-              size='lg'
-              focusBorderColor='gray.100'
+              size={["sm", "md", "lg"]}
+              focusBorderColor="gray.100"
               color={colors.gray600}
               textColor={colors.gray600}
               background={colors.background}
             />
 
             <Input
-              id='email'
+              id="email"
               placeholder="E-mail"
-              {...register('email')}
+              {...register("email")}
               isRequired
               isInvalid={errors.email}
               onInvalid={() => onValidating(errors.email)}
-              size='lg'
-              focusBorderColor='gray.100'
+              size={["sm", "md", "lg"]}
+              focusBorderColor="gray.100"
               color={colors.gray600}
               textColor={colors.gray600}
               background={colors.background}
             />
 
-            <InputGroup size='lg'>
+            <InputGroup>
               <Input
-                id='password'
-                placeholder='Senha'
-                {...register('password')}
+                id="password"
+                placeholder="Senha"
+                {...register("password")}
                 isRequired
                 isInvalid={errors.password}
                 onInvalid={() => onValidating(errors.password)}
-                size={"lg"}
-                focusBorderColor='gray.100'
+                size={["sm", "md", "lg"]}
+                focusBorderColor="gray.100"
                 color={colors.gray600}
                 textColor={colors.gray600}
-                type={show ? 'text' : 'password'}
+                type={show ? "text" : "password"}
                 background={colors.background}
               />
-              <InputRightElement width='4.5rem'>
-                <Button
-                  size='md'
+              <InputRightElement>
+                <IconButton
                   onClick={setShow.toggle}
                   color={colors.main}
-                  colorScheme="whiteAlpha"
-                >
-                  {!show ?
-                    <RiEye2Line size={30} /> :
-                    <RiEyeCloseLine size={30} />}
-                </Button>
+                  background={"transparent"}
+                  icon={!show ? <RiEye2Line /> : <RiEyeCloseLine />}
+                />
               </InputRightElement>
             </InputGroup>
 
-            <InputGroup size='lg'>
+            <InputGroup>
               <Input
-                id='repeatPassword'
-                placeholder='Confirmar senha'
-                {...register('repeatPassword')}
+                id="repeatPassword"
+                placeholder="Confirmar senha"
+                {...register("repeatPassword")}
                 isRequired
                 isInvalid={errors.repeatPassword}
                 onInvalid={() => onValidating(errors.repeatPassword)}
-                size={"lg"}
-                focusBorderColor='gray.100'
+                size={["sm", "md", "lg"]}
+                focusBorderColor="gray.100"
                 color={colors.gray600}
                 textColor={colors.gray600}
-                type={showConfirmation ? 'text' : 'password'}
+                type={showConfirmation ? "text" : "password"}
                 background={colors.background}
               />
-              <InputRightElement width='4.5rem'>
-                <Button
-                  size='md'
+              <InputRightElement>
+                <IconButton
                   onClick={setShowConfirmation.toggle}
                   color={colors.main}
-                  colorScheme="whiteAlpha"
-                >
-                  {!showConfirmation ?
-                    <RiEye2Line size={30} /> :
-                    <RiEyeCloseLine size={30} />}
-                </Button>
+                  background={"transparent"}
+                  icon={!showConfirmation ? <RiEye2Line /> : <RiEyeCloseLine />}
+                />
               </InputRightElement>
             </InputGroup>
-
-
           </InputGroup>
 
-          <Button
-            type="submit"
-            backgroundColor={colors.black}
-            colorScheme="gray"
-            color={colors.white}
-            marginTop={'3rem'}
-            marginBottom={3}
-            isLoading={signUpLoading}
-          >Inscrever</Button>
-          <Button
-            to={'/sign-in'}
-            as={Link}
-            backgroundColor={colors.teal900}
-            colorScheme="gray"
-            color={colors.white}
-            isDisabled={isSubmitting}
-          >
-            Já possuo conta
-          </Button>
-        </Form>
+          <Stack spacing={[2, 4]}>
+            <Button
+              type="submit"
+              backgroundColor={colors.black}
+              colorScheme="gray"
+              color={colors.white}
+              isLoading={signUpLoading}
+              size={["sm", "md", "lg"]}
+            >
+              Inscrever
+            </Button>
+            <Button
+              to={"/sign-in"}
+              as={Link}
+              backgroundColor={colors.teal900}
+              colorScheme="gray"
+              color={colors.white}
+              isDisabled={isSubmitting}
+              size={["sm", "md", "lg"]}
+            >
+              Já possuo conta
+            </Button>
+          </Stack>
+        </Stack>
       </form>
     </SignPage>
-  )
-}
-
-const Form = styled(FormControl)`
-  display: flex;
-  flex-direction: column;
-
-  Link {
-    width: 100%;
-  }
-`
+  );
+};
 
 export default SignUp;
